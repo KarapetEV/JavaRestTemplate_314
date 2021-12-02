f7cc9ee9766b6f87994826c3cbe0c272346904f2
Evgeniy Karapyshev <karapet2011@gmail.com>
2021-12-02 15:06:59 +0300

    Заготовка

---
+++ src/main/java/org/example/javaresttemplate/jararesttemplate_314/JaraRestTemplate314Application.java
@@ -0,0 +1,13 @@
+package org.example.javaresttemplate.jararesttemplate_314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaraRestTemplate314Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaraRestTemplate314Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaresttemplate/jararesttemplate_314/JaraRestTemplate314ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaresttemplate.jararesttemplate_314;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaraRestTemplate314ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
